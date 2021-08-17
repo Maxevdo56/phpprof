f1695eb6d258d5e2b2df1d2ecd84b84d714fdd28
maxevdo56 <Maxevdo56@yandex.ru>
2021-08-17 18:03:58 +0300

    lesson2

--- index.php
+++ index.php
@@ -1,12 +1,102 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+
+abstract class Product 
+{
+    public $price;
+    public $quantity;
+
+    abstract public function calcPrice();
     
-</body>
-</html>
+    abstract public function calcTotalSales();
+    
+    public function __construct($price, $quantity) {
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+};
+
+
+class RealProduct extends Product 
+{
+    public function calcPrice() : float
+    {
+        return $this->price;
+    }
+    public function calcTotalSales() : float
+    {
+        return ($this->calcPrice() * $this->quantity);
+    }
+};
+
+$real_prod = new RealProduct(100, 5);
+echo "Total sales for RealProduct = ".$real_prod->calcTotalSales()."<hr>";
+
+
+class DigitalProduct extends Product 
+{
+    public function calcPrice() : float
+    {
+        return ($this->price / 2);
+    }
+    public function calcTotalSales() : float
+    {
+        return ($this->calcPrice() * $this->quantity);
+    }
+};
+
+$digital_prod = new DigitalProduct(100, 5);
+echo "Total sales for DigitalProduct = ".$digital_prod->calcTotalSales()."<hr>";
+
+
+class WeightProduct extends Product 
+{
+    public $weight; // вес в кг
+
+    public function __construct($price, $quantity, $weight) {
+        parent::__construct($price, $quantity);
+        $this->weight = $weight;
+    }
+
+    public function calcPrice() : float
+    {
+        return ($this->price * $this->weight);
+    }
+    
+    public function calcTotalSales() : float
+    {
+        return ($this->calcPrice() * $this->quantity);
+    }
+};
+
+$weight_prod = new WeightProduct(100, 5, 1.3);
+echo "Total sales for WeightProduct = ".$weight_prod->calcTotalSales()."<hr>";
+
+
+/************************************/
+/***   Задание 2. со звёздочкой   ***/
+/************************************/
+
+trait SingletonTrait 
+{
+    public static $var;
+    public function __construct() { } 
+    public function __clone() { }
+    public function __wakeup()  { }
+    public static function getInstance() 
+    {
+        if ( self::$var == null ) {
+        self::$var = new self();
+    }
+        return self::$var;
+    }
+    public function someAction() { }   
+    
+}
+
+class Singleton {
+    use SingletonTrait;
+}
+
+Singleton::getInstance()->someAction(); 
+
+?>
